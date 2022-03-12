fa042bd3b67b0d8ff8a8ad7e8728f4e35ed7107b
Luis Aaron Blas <92341949+blasluis@users.noreply.github.com>
2022-03-12 17:06:59 -0500

    Books Index/Create/New

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,21 @@ const express = require('express')
 const router = express.Router()
 
 const authorsRouter = require('./authors')
+const booksRouter = require('./books')
 
-router.get('/', (req, res) => {
-    res.render('index')
+const Book = require('../models/book')
+
+router.get('/', async (req, res) => {
+    let books
+    try {
+        books = await Book.find().sort({createdAt: 'desc'}).limit(10).exec()
+    } catch {
+        books = []
+    }
+    res.render('index', {books: books})
 })
 
-router.use('/authors',authorsRouter)
+router.use('/authors', authorsRouter)
+router.use('/books', booksRouter)
 
 module.exports = router
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.set('layout', 'layouts/layout')     // Directorio de plantillas (Header y Fo
 
 app.use(expressLayouts)                 // LLamar a la dependencia de manejo de plantillas con ejs
 app.use(express.static('public'))       // Directorio de los archivos estaticos css,js,img
-app.use(bodyParser.urlencoded({ limit: '10mb', extended:false })) //configurar el urlencoded para capturar datos del formulario
+app.use(express.urlencoded({ limit: '10mb', extended:false })) //configurar el urlencoded para capturar datos del formulario
 app.use(express.json())
 
 // Conectando a la Base de Datos
