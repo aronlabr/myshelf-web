9f9f199d1e8a3d491ce684a43fa216b6f9b31300
Luis Aaron Blas <92341949+blasluis@users.noreply.github.com>
2022-03-13 14:43:43 -0500

    Carga de portadas directamente a la DB

--- models/book.js
+++ models/book.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 const Author = require('./author')
-const path = require('path')
-const coverImageBasePath = 'uploads/bookCovers'
 
 const bookSchema = new mongoose.Schema({
     title: {type: String, required: true },
@@ -9,15 +7,15 @@ const bookSchema = new mongoose.Schema({
     pageCount: {type: Number, required: true },
     publishDate: {type: Date, required: true },
     createdAt: {type: Date, required: true, default: Date.now() },
-    coverImageName: {type: String, required: true },
+    coverImage: {type: Buffer, required: true },
+    coverImageType: {type: String, required: true},
     author: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Author'}
 })
 
 bookSchema.virtual('coverImagePath').get(function() {
-    if (this.coverImageName != null) {
-        return path.join('/',coverImageBasePath,this.coverImageName)
+    if (this.coverImage != null && this.coverImageType != null) {
+        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
     }
 })
 
 module.exports = mongoose.model('Book', bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
