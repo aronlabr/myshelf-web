a1ac27815a528dba04ba58cf7df7f42d6193ddab
Luis Aaron Blas <92341949+blasluis@users.noreply.github.com>
2022-03-19 18:07:15 -0500

    Terminando proyecto :D

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,26 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-)
+const rootStyles = window.getComputedStyle(document.documentElement)
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150,
-})
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null &&
+    rootStyles.getPropertyValue('--book-cover-width-large') != '') {
+    ready()
+} else {
+    document.getElementById('main-css').addEventlistener('DOMContentLoaded', ready)
+}
+function ready() {
+    const coverwidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverwidth / coverAspectRatio
 
-FilePond.parse(document.body);
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    )
+
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverwidth,
+        imageResizeTargetHeight: coverHeight,
+    })
+    FilePond.parse(document.body)
+}
